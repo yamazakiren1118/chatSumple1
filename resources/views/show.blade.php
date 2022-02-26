75d9d53ae96ef87bfc5aa643b6f16a15b2512fc9
yamazakiren1118 <yamazakiren1118@yahoo.co.jp>
2022-02-26 23:30:39 +0900

    heroku

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -37,7 +37,7 @@
   <!-- <script src="http://localhost:3000/socket.io/socket.io.js"></script> -->
   <script src="https://polar-retreat-24639.herokuapp.com/socket.io/socket.io.js"></script>
   <script>
-    var socket = io.connect("https://polar-retreat-24639.herokuapp.com/socket.io/socket.io.js",{secure:true});
+    var socket = io("https://polar-retreat-24639.herokuapp.com");
     // var socket = io("http://localhost:3000");
     var id = "{{$id}}";
   </script>
